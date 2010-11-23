f439a9d3f146516bed877aa92bcbc72caffcf8b3
Graham <graham-git@stormbrew.ca>
2010-11-22 21:15:41 -0700

    Adding the Table::Proxy class which does some mojo around Lazy::Promise for promised table results.
    In the long run, the intent is to get rid of Lazy in favour of directly implementing lazy evaluation of query fetches. This code is extremely obscure and it's difficult to grasp its purpose.

---
+++ spec/table/proxy_spec.rb
@@ -0,0 +1,3 @@
+# This is just a placeholder. Actually properly specing Table::Proxy is a little difficult,
+# and the intention is to replace the use of Lazy with a more specific implementation later
+# anyways.
